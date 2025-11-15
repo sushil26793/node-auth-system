d4c0efe153334b413a62251c04983c3269b1d5ba
sushil satyarthi <sushilsatyarthi@gmail.com>
2025-11-15 21:57:02 +0530

    zod-validation-added

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authController } from "../controllers/authController";
 import { authenticateToken } from "../middlewares/authenticate";
 import { loginLimiter, passwordResetLimiter, registerLimiter } from "../middlewares/rateLImit";
+import { changePasswordSchema, loginSchema, refreshTokenSchema, registerSchema, validateRequest } from "../middlewares/validation";
 
 
 const router = Router();
@@ -9,12 +10,12 @@ const router = Router();
 
 
 
-router.post('/register',registerLimiter, authController.register);
-router.post('/login',loginLimiter ,authController.login);
-router.post('/refresh-token',authController.refreshToken);
-router.post('/logout',authenticateToken, authController.logout);
-router.get('/me',authenticateToken, authController.getCurrentUser);
-router.post('/change-password',authenticateToken,passwordResetLimiter,authController.changePassword);
+router.post('/register', validateRequest(registerSchema), registerLimiter, authController.register);
+router.post('/login', validateRequest(loginSchema), loginLimiter, authController.login);
+router.post('/refresh-token', validateRequest(refreshTokenSchema), authController.refreshToken);
+router.post('/logout', authenticateToken, authController.logout);
+router.get('/me', authenticateToken, authController.getCurrentUser);
+router.post('/change-password', validateRequest(changePasswordSchema), authenticateToken, passwordResetLimiter, authController.changePassword);
 
 
 export default router;
